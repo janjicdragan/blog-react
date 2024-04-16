42e06380eaaf93681dee4fbc2b0be56f284501e4
Dragan <janjic.drgn@gmail.com>
2024-04-16 18:32:08 +0200

    add main fetching function

---
+++ src/shared/services/dataFetcher/dataFetcher.ts
@@ -0,0 +1,19 @@
+export async function fetchData<T>(method: string, url: string): Promise<T> {
+  try {
+    let options: RequestInit = {
+      method: method,
+    };
+
+    const response = await fetch(url, options);
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch data, status: ${response.status}`);
+    }
+
+    const data: T = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    throw error;
+  }
+}
