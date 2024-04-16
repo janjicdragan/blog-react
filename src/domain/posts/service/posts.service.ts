39307e5cb275d707f1d453bbb299a09941168a68
Dragan <janjic.drgn@gmail.com>
2024-04-16 19:30:57 +0200

    add posts service layer for data fetching

---
+++ src/domain/posts/service/posts.service.ts
@@ -0,0 +1,24 @@
+import { API_BASE_URL } from '../../../shared/constants';
+import { fetchData } from '../../../shared/services/dataFetcher/dataFetcher';
+import { Post } from '../types/interfaces';
+
+async function get() {
+  const url = `${API_BASE_URL}/posts`;
+  return await fetchData<Post[]>('get', url);
+}
+
+async function getPaginated(page: number) {
+  const url = `${API_BASE_URL}/posts?_page=${page}`;
+  return await fetchData<Post[]>('get', url);
+}
+
+async function getByUserId(userId: number) {
+  const url = `${API_BASE_URL}/posts?userId=${userId}`;
+  return await fetchData<Post[]>('get', url);
+}
+
+export const PostsService = {
+  get,
+  getByUserId,
+  getPaginated,
+};
